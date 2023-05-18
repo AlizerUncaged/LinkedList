de8c870fc12f6c4a8337bb8382c24b2f39bc9236
gaming <saylent1@gmail.com>
2023-05-18 12:00:07 +0800

    ✨ Added more test and LinkedList functions.

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -12,8 +12,15 @@ public class LinkedList {
         this.head = head;
     }
 
+    public Node getTail() {
+        return this.get(this.getSize() - 1);
+    }
 
     public Node get(long index) {
+
+        if (index > size - 1 || isEmpty())
+            return null;
+
         var currentNode = head;
 
         for (int i = 0; i < index; i++)
@@ -22,17 +29,21 @@ public class LinkedList {
         return currentNode;
     }
 
-//    public void addToTail(Node newNode)
-//    {
-//
-////                var currentNode = head;
-////                for(int i = 0; i < (size - 1); i++)
-////                    currentNode = currentNode.getNext();
-////
-////                currentNode.setNext(newNode);
-////
-//    }
-//
+    public void addToTail(Node newNode) {
+        if (isEmpty()) // empty list
+            setHead(newNode);
+        else {
+            var currentNode = head;
+
+            for (int i = 0; i < (size - 1); i++)
+                currentNode = currentNode.getNext();
+
+            currentNode.setNext(newNode);
+        }
+
+        size++;
+    }
+
     public void addToFront(Node newNode) {
         if (size == 0) // empty list
             setHead(newNode);
@@ -45,4 +56,32 @@ public class LinkedList {
         size++;
     }
 
+    public void removeFromFront() {
+
+        if (isEmpty())
+            return;
+
+        head = head.getNext();
+        size--;
+    }
+
+    public void removeTail() {
+
+        if (isEmpty())
+            return;
+
+        var currentNode = head;
+
+        for (int i = 0; i < size - 2; i++) {
+            currentNode = currentNode.getNext();
+        }
+
+        currentNode.setNext(null);
+
+        size--;
+    }
+
+    public boolean isEmpty() {
+        return head == null || size == 0;
+    }
 }
--- src/LinkedListTest.java
+++ src/LinkedListTest.java
@@ -3,20 +3,109 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LinkedListTest {
+    String[] nodeValues = { "Ashe", "Pyke", "Yasuo", "Warwick", "Master Yi" };
+
     private LinkedList linkedList = new LinkedList();
 
     @Test
-    public void shouldAddNode() {
-        String[] nodeValues = { "Ashe", "Pyke", "Yasuo"};
+    // Add and remove test
+    public void shouldAddAndRemove() {
+        populateList(true);
 
-        long expectedSize = 0;
-        for (String y : nodeValues) {
-            linkedList.addToFront(new Node(y));
-            expectedSize++;
+        linkedList.removeTail();
+        linkedList.removeFromFront();
 
-            // Compare size.
-            assertEquals(expectedSize, linkedList.getSize());
+        // Compare LinkedList and nodeValues size.
+        assertEquals(nodeValues.length - 2, linkedList.getSize());
+
+        String[] addChampions = { "Trundle", "Jinx", "Teemo", "Soraka" };
+
+        for(String champion : addChampions)
+        {
+            linkedList.addToTail(new Node(champion));
+
+            // Compare last item.
+            assertEquals(champion, linkedList.getTail().getValue());
         }
+    }
+
+    @Test
+    public void shouldRemoveFromTail()
+    {
+        populateList(true);
+
+        linkedList.removeTail();
+
+        var lastElement = linkedList.getTail();
+
+        // Compare LinkedList last element to nodeValues' second to the last element.
+        assertEquals(nodeValues[nodeValues.length - 2], lastElement.getValue());
+
+        // Check size, LinkedList size should be one less than nodeValues.
+        assertEquals(nodeValues.length - 1, linkedList.getSize());
+    }
+
+    @Test
+    public void shouldRemoveFromTailEmpty()
+    {
+        linkedList.removeTail();
+
+        var lastElement = linkedList.get(linkedList.getSize() - 1);
+
+        // Compare LinkedList last element to nodeValues' second to the last element.
+        assertEquals(null, lastElement);
+
+        // Check size, LinkedList size should be one less than nodeValues.
+        assertEquals(0, linkedList.getSize());
+    }
+
+    @Test
+    public void shouldRemoveFromFront() {
+        populateList(true);
+
+        linkedList.removeFromFront();
+
+        var firstElement = linkedList.get(0);
+
+        // Compare LinkedList first element to nodeValues second element.
+        assertEquals(nodeValues[1], firstElement.getValue());
+
+        // Check size, LinkedList size should be one less than nodeValues.
+        assertEquals(nodeValues.length - 1, linkedList.getSize());
+    }
+
+    @Test
+    public void shouldRemoveFromFrontEmpty() {
+        linkedList.removeFromFront();
+
+        var firstElement = linkedList.get(0);
+
+        // Compare LinkedList first element to nodeValues second element.
+        assertEquals(null, firstElement);
+
+        // Check size, LinkedList size should be one less than nodeValues.
+        assertEquals(0, linkedList.getSize());
+    }
+
+    @Test
+    public void shouldAddNodeToTail() {
+        populateList(true);
+
+        // Add to front adds to index 0.
+        // Reverse for loop.
+        for (int i = 0; i < nodeValues.length; i++) {
+
+            // Compare each value in LinkedList and String[]
+            assertEquals(nodeValues[i], linkedList.get(i).getValue());
+        }
+
+        // Now check size.
+        assertEquals(nodeValues.length, linkedList.getSize());
+    }
+
+    @Test
+    public void shouldAddNodeToHead() {
+        populateList(false);
 
         // Add to front adds to index 0.
         // Reverse for loop.
@@ -29,5 +118,21 @@ class LinkedListTest {
 
         // Now check size.
         assertEquals(nodeValues.length, linkedList.getSize());
+    }
+
+    public void populateList(boolean addLocation) {
+        long expectedSize = 0;
+
+        for (String y : nodeValues) {
+            if (addLocation)
+                linkedList.addToTail(new Node(y));
+            else
+                linkedList.addToFront(new Node(y));
+
+            expectedSize++;
+
+            // Compare size.
+            assertEquals(expectedSize, linkedList.getSize());
+        }
     }
 }
